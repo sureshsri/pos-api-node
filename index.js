810e23811780c559e3e3e74b3707d88f928ef7b7
suresh <suresh.sri248@gmail.com>
2024-05-05 08:45:47 +0530

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+---------------------------
+what we need to instal
+---------------------------
+*express.js(npm i express)
+*mongoose(npm i momgoose)
+*nodemon (npm i nodemon -g)
+*dotenv(npm i dotenv)
+*body-parser(npm i body-paser)
+*bcrypt(npm i bcrypt)
+*jsonwebtoken(npm i jsonwebtoken) 
+*/
